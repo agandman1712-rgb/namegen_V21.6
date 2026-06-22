d4bbf4dabb4ea2035271d6ff8fc7e9a73fd25f97
agandman1712-rgb <agandman.1712@gmail.com>
2026-06-23 00:45:39 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,16 +11,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_default_vpc" "default" {
+resource "aws_vpc" "namegen_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
-    Name = "Default VPC"
+    Name = "namegen-custom-vpc"
   }
 }
 
-resource "aws_default_subnet" "default_az1" {
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.namegen_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "namegen-public-1"
+  }
 }
 
-resource "aws_default_subnet" "default_az2" {
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.namegen_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "namegen-public-2"
+  }
 }
