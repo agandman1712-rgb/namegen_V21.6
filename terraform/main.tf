600d1b4c730d8bfef3f2ac8be38353f8919a0b09
agandman1712-rgb <agandman.1712@gmail.com>
2026-06-27 23:17:47 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -219,7 +219,7 @@ resource "aws_eks_access_entry" "pipeline_access" {
 
 resource "aws_eks_access_policy_association" "admin_policy" {
   cluster_name  = aws_eks_cluster.namegen_cluster.name
-  policy_arn    = "arn:aws:iam::aws:policy/AmazonEKSClusterAdminPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = aws_iam_role.eks_role.arn
 
   access_scope {
