bc2c216a3048b9ebfe8b4481ca24542f6da773fd
agandman1712-rgb <agandman.1712@gmail.com>
2026-07-13 19:53:18 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -194,7 +194,7 @@ resource "aws_iam_role" "eks_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "://amazonaws.com"
+          Service = "eks.amazonaws.com"
         }
       }
     ]
@@ -237,7 +237,7 @@ resource "aws_iam_role" "eks_node_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "://amazonaws.com"
+          Service = "ec2.amazonaws.com"
         }
       }
     ]
